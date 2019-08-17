5da3a5e1aa51805ab27f40aae3aee0dfca5e4571
Liran Tal <liran.tal@gmail.com>
2019-08-17 22:53:04 +0300

    feat: display a random tweet

--- src/components/SpeakingTip.vue
+++ src/components/SpeakingTip.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="speakingTip">
-    <TwitterCard v-bind:tweet-url="tweet"></TwitterCard>
+    <TwitterCard v-bind:tweet-url="randomTweet"></TwitterCard>
   </div>
 </template>
 
@@ -12,11 +12,18 @@ export default {
   components: {
     TwitterCard
   },
-  data: function() {
-    return {
-      tweet:
-        "https://twitter.com/the_thagomizer/status/930910860846993410?ref_src=twsrc%5Etfw"
-    };
+  computed: {
+    randomTweet: function() {
+      const tweets = [
+        "https://twitter.com/the_thagomizer/status/930910860846993410?ref_src=twsrc%5Etfw",
+        "https://twitter.com/devolore/status/950453575817162752?ref_src=twsrc%5Etfw"
+      ];
+
+      const totalTweets = tweets.length;
+      const randomTweetIndex = Math.floor(Math.random() * totalTweets);
+
+      return tweets[randomTweetIndex];
+    }
   }
 };
 </script>

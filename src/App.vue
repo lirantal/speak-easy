e331605defef41e86fa87888a16eb9c80647b3c5
Liran Tal <liran.tal@gmail.com>
2019-08-26 08:19:34 +0300

    feat: navbar and favicon

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="#">speak|easy</a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
+
     <section class="hero is-dark">
       <div class="hero-body">
         <SpeakingTip />
@@ -10,7 +22,7 @@
       <footer class="footer">
         <div class="content has-text-centered">
           <p>
-            <strong>Speak Easy</strong> by
+            <strong>speak|easy</strong> by
             <a href="https://twitter.com/liran_tal">Liran Tal</a>.
           </p>
         </div>

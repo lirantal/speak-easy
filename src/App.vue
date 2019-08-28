8d4f72d465ac85462afcc675ba5b288ac62ef0fd
Liran Tal <liran.tal@gmail.com>
2019-08-28 22:36:22 +0300

    fix: clean up menu

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,6 @@
     <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="#">speak|easy</a>
-
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
       </div>
     </nav>
 
